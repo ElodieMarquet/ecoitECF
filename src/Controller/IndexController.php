6905b895cca7de221a553a78ed59c217aea75ab1
Elodie Marquet <elmarquet@gmail.com>
2022-03-27 16:32:21 +0200

    reset git

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+<<<<<<< HEAD
+    #[Route('/index', name: 'app_index')]
+=======
+    #[Route('/accueil', name: 'app_index')]
+>>>>>>> master
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+}
