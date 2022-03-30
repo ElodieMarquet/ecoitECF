a064943ea31828bc6262c170244601eeab8ce7f9
Elodie Marquet <elmarquet@gmail.com>
2022-03-30 15:15:56 +0200

    create formation file

---
+++ src/Controller/FormationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Formation;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class FormationController extends AbstractController
+{
+    #[Route('/{slug}', name: 'app_detail')]
+    public function detail(Formation $forma): Response
+    {
+        return $this->render('formation/index.html.twig', compact('forma'));
+    }
+}
