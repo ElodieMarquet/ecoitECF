9b394d6595036c286973b78658106cb5c14c6eb3
Elodie Marquet <elmarquet@gmail.com>
2022-04-02 01:17:41 +0200

    update style file

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -23,7 +23,7 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter les CGU',
                     ]),
                 ],
                 'label' => 'J\'accpte les Condition Générales d\'Utilisation'
