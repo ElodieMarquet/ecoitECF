292a774503e6b56d6a35b38fafcc8ac71842c33c
Elodie Marquet <elmarquet@gmail.com>
2022-04-04 21:16:39 +0200

    update contact file

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,7 +4,8 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,13 +15,13 @@ class ContactType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class)
-            ->add('nom')
-            ->add('message')
-            ->add('Envoyer', SubmitType::class)
+            ->add('nom', TextType::class)
+            ->add('message', TextareaType::class, ['attr' => ['row' => 6], ])
+            
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             // Configure your form options here
