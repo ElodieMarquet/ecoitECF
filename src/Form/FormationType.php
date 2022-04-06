501e1f8218b90942b376b5adb49d17bacfdcd080
Elodie Marquet <elmarquet@gmail.com>
2022-04-06 13:41:16 +0200

    layout create file

--- src/Form/FormationType.php
+++ src/Form/FormationType.php
@@ -7,6 +7,8 @@ use App\Entity\Section;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
@@ -16,7 +18,7 @@ class FormationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class)
             ->add('image', FileType::class, [
                 'label' => 'Image au format jpeg ou png',
                 'mapped' =>false,
@@ -33,8 +35,8 @@ class FormationType extends AbstractType
                 ],
 
             ])
-            ->add('description')
-            ->add('contenu')
+            ->add('description', TextType::class)
+            ->add('contenu', TextareaType::class)
             ->add('video', FileType::class, [
                 'label' => 'Video au format mp4',
                 'mapped' =>false,
@@ -49,7 +51,6 @@ class FormationType extends AbstractType
                 ],
 
             ])            
-            ->add('slug')
             ->add('section', EntityType::class, [
                 'class' => Section::class,
                 'allow_extra_fields' => true,
