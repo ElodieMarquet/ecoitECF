09c250e23f4e11118b039be88401adceafe1aa0f
Elodie Marquet <elmarquet@gmail.com>
2022-04-06 12:54:34 +0200

    layout moncompte

--- src/Controller/MoncompteController.php
+++ src/Controller/MoncompteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class MoncompteController extends AbstractController
 {
     #[Route('/moncompte', name: 'app_moncompte')]
-    public function index(): Response
+    public function details(): Response
     {
-        return $this->render('moncompte/index.html.twig', [
-            'controller_name' => 'MoncompteController',
-        ]);
+        return $this->render('moncompte/index.html.twig');
     }
 }
